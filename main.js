4a8bec7bde1ccd12f9e0fee5c0a2ac10a1849728
linkt <mr.tobtv@gmail.com>
2024-02-15 07:55:12 +0100

    Refactor cartName and baseUrl into settings object

--- main.js
+++ main.js
@@ -1,49 +1,41 @@
-const cartName = "INSERT-CART-NAME-HERE"; // mit Deinem Warenkorb-Namen ersetzen
-
-// AB HIER MUSS NICHTS MEHR ANGEPASST WERDEN
-const baseUrl = "https://api.preisrunter.net/v1/cart/";
+const settings = {
+    cartName: "INSERT-CART-NAME-HERE", // mit Deinem Warenkorb-Namen ersetzen
+    baseUrl: "https://api.preisrunter.net/v1/cart/"
+};
 
 async function loadCartData(cartName) {
+    const urlWithCartName = `${settings.baseUrl}?q=${encodeURIComponent(cartName)}`;
     try {
-        const urlWithCartName = `${baseUrl}?q=${encodeURIComponent(cartName)}`;
-        const request = new Request(urlWithCartName);
-        request.method = "GET";
- 
-        request.headers = {"Content-Type": "application/json"};
- 
-        const response = await request.loadJSON();
- 
-        if (response && response.products) {
-            return response.products;
-        } else {
-            console.log("Keine Einträge im Warenkorb.");
-            return [];
-        }
+        const response = await fetch(urlWithCartName, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+        const data = await response.json();
+        return data.products || [];
     } catch (error) {
         console.error("Fehler beim Laden der Daten: ", error);
         return [];
     }
 }
- 
+
 async function createWidget(products) {
     let widget = new ListWidget();
- 
-    if (products.length > 0) {
-        products.forEach(product => {
-            let productLine = widget.addText(`${product.productName} - ${product.productPrice} € - ${product.productMarket}`);
-            productLine.font = Font.systemFont(12);
-            widget.addSpacer(4);
-        });
-    } else {
+    if (products.length == 0) {
         widget.addText("Keine Einträge im Warenkorb.");
+        return widget;
     }
+    products.forEach(product => {
+        const productLine = widget.addText(`${product.productName} - ${product.productPrice} € - ${product.productMarket}`);
+        productLine.font = Font.systemFont(12);
+        widget.addSpacer(4);
+    });
     return widget;
 }
- 
+
 async function main() {
     if (config.runsInWidget) {
-        const products = await loadCartData(cartName);
-        let widget = await createWidget(products);
+        const products = await loadCartData(settings.cartName);
+        const widget = await createWidget(products);
         Script.setWidget(widget);
     } else {
         console.log("Dieses Script funktioniert nur als Widget und nicht direkt in Scriptable.");
